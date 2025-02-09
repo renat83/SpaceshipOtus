9cc278d6a2cb20601544fa8b67c4a891ccf76556
Sabitov Renat <Renat.Sabitov@russianpost.ru>
2025-02-09 19:57:37 +0300

    Тренировочный код

--- SpaceshipOtus/Homework3/Training.cs
+++ SpaceshipOtus/Homework3/Training.cs
@@ -1,4 +1,6 @@
-﻿namespace SpaceshipOtus.Homework3
+﻿using System.Collections.Concurrent;
+
+namespace SpaceshipOtus.Homework3
 {
     public interface IBase
     {
@@ -26,19 +28,10 @@
 
         public void Dispose()
         {
-            Dispose(true);
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
             if (!_disposed)
             {
-                if (disposing)
-                {
-                    // Освобождаем управляемые ресурсы
-                    Console.WriteLine("Внешний ресурс освобожден.");
-                }
-
+                // Освобождаем управляемые ресурсы
+                Console.WriteLine("Внешний ресурс освобожден.");
                 // Освобождаем неуправляемые ресурсы (если есть)
                 _disposed = true;
             }
@@ -58,6 +51,7 @@
             }
             catch (Exception)
             {
+                //handler
                 throw;
             }
             finally
@@ -65,5 +59,56 @@
                 r.Dispose();
             }
         }
+
+        public void Method(IBase b)
+        {
+            using (Resource r1 = new Resource())
+            {
+                b.Function(r1);
+            }
+        }
     }
+
+    public interface IComand
+    {
+        void Execute();
+    }
+
+    public class Temp
+    {
+        BlockingCollection<IComand> q = new BlockingCollection<IComand>();
+
+        public void MyFunc() 
+        {
+            var stop = false;
+
+            while (!stop)
+            {
+                IComand cmd = q.Take();
+
+                try
+                {
+                    cmd.Execute();
+                }
+                catch (Exception ex)
+                {
+                    ExceptionHandler.Handle(cmd, ex).Execute();
+                }
+                
+            }
+        }
+    }
+
+    public class ExceptionHandler
+    {
+        public ExceptionHandler() 
+        { 
+        }
+
+        public static IComand Handle(IComand cmd, Exception ex)
+        {
+
+        }
+    }
+
 }
